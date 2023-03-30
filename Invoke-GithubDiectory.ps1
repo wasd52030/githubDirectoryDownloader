6ec721fbb22f2c6a07072a94a7a0f53dcb90ce7d
sobol <nppstcc1001050311@gmail.com>
2023-03-30 22:55:59 +0800

    repository init

---
+++ Invoke-GithubDiectory.ps1
@@ -0,0 +1,83 @@
+function praseInfo($url) {
+    $splitPath = $url -split "/" | Select-Object -Skip 3
+    Write-Host $splitPath
+
+    $info = @{}
+    $info["author"] = "$($splitPath[0])"
+    $info["repository"] = "$($splitPath[1])"
+    $info["branch"] = ($splitPath[3]) ? "$($splitPath[3])" : "master"
+    $info["directory"] = "$($splitPath[$splitPath.Length - 1])"
+    $info["rootUrl"] = ($splitPath[3])?
+    "https://github.com/$($splitPath[0])/$($splitPath[1])/tree/$($splitPath[3])" :
+    "https://github.com/$($splitPath[0])/$($splitPath[1])"
+
+    return $info
+}
+
+function getInfoUrl($author, $repository, $branch) {
+    return  "https://api.github.com/repos/$($author)/$($repository)/git/trees/$($branch)?recursive=1"
+}
+
+function getDownloadUrl($author, $repository, $branch, $path) {
+    return  "https://raw.githubusercontent.com/$($author)/$($repository)/$($branch)/$($path)"
+}
+
+function iterDirectory($repoInfo) {
+    $infoUrl = getInfoUrl $repoInfo["author"] $repoInfo["repository"] $repoInfo["branch"]
+    
+    $res = $res = (Invoke-WebRequest $infoUrl).Content | ConvertFrom-Json
+    if ($repoInfo["repository"] -eq $repoInfo["directory"]) {
+        $res = $res.tree
+    }
+    else {
+        $res = $res.tree | Where-Object { $_.path.Contains($repoInfo["directory"]) }
+    }
+
+    $res | ForEach-Object {
+        $item = $_
+        $paths = $item.path -split "/"
+        $path = ""
+        if ($repoInfo["repository"] -eq $repoInfo["directory"]) {
+            $path = "$($repoInfo["repository"])/$($item.path)"
+        }
+        else {
+            if ([array]::IndexOf($paths, $repoInfo["directory"]) -ne 0) {
+                $i = [array]::IndexOf($paths, $repoInfo["directory"])
+                $path = ($paths | Select-Object -Skip $i) -join "/"
+            }
+            else {
+                $path = $paths -join "/"
+            }
+        }
+
+        if ($item.type -eq "tree") {
+            New-Item -Force -ItemType "directory" -Path $path 
+        }
+        elseif ($item.type -eq "blob") {
+            $dwUrl = getDownloadUrl $repoInfo["author"] $repoInfo["repository"] $repoInfo["branch"] $item.path
+            # Invoke-WebRequest -Uri $dwUrl -OutFile $path
+            Start-ThreadJob -ScriptBlock {
+                param($url, $path)
+                Write-Host "$($path) download start"
+                Invoke-WebRequest -Uri $url -OutFile $path | Out-Null
+                Write-Host "$($path) download end"
+            } -StreamingHost $Host -ArgumentList $dwUrl, $path
+        }
+    }
+}
+
+function Invoke-GithubDirectory($url) {
+    if ($url -ne "" ) {
+        if ($url -ne $null) {
+            Set-Location "~\downloads"
+            $info = praseInfo $url
+            New-Item -Force -ItemType "directory" -Path "./$($info["directory"])"
+            iterDirectory $info
+        }
+    }
+}
+
+Invoke-GithubDirectory
+
+# sample
+# Invoke-GithubDirectory -url "https://github.com/wasd52030/pose-monitor"
---
+++ readme.md
@@ -0,0 +1,36 @@
+# githubDirectoryDownloader
+
+一個用來下載**github**資料夾的小工具，以**powershell**撰寫
+
+- 版本要求: **powershell7$\uparrow$**
+- 目前只支援下載資料夾，單檔下載可以直接從github網頁上面獲得
+
+## 安裝
+
+- 安裝powershell 7(已安裝可以跳過)
+
+- clone本專案，並放到你喜歡的地方
+
+    ```
+    git clone https://github.com/wasd52030/githubDirectoryDownloader
+    ```
+
+- 開啟user profile(編輯器自選)
+
+    ```
+    notepad $PROFILE
+    ```
+    
+- 載入指令碼
+
+  ```
+  Import-Module -Name "yourpath\githubDirectoryDownloader\Invoke-GithubDiectory.ps1"
+  ```
+
+ ## 使用
+指令格式如下，**githuburl**為你想下載的github repository網址(資料夾網址亦可)
+ ```
+Invoke-GithubDirectory -url githuburl
+ ```
+
+例如: ``Invoke-GithubDirectory -url https://github.com/wasd52030/githubDirectoryDownloader``
